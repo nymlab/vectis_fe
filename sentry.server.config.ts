1745f326b57a2787717eb99cec7e948f1d76ce5b
j0nl1 <jonlitech@gmail.com>
2022-07-13 11:31:18 +0200

    fix: sentry environment

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,11 +1,12 @@
 import * as Sentry from "@sentry/nextjs";
+import pkj from "./package.json";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-const PKJ_VERSION = process.env.npm_package_version;
+const ENV = process.env.VERCEL_ENV || "local";
 
 Sentry.init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
-  environment: "local",
-  release: `vectis-dapp@${PKJ_VERSION}`,
+  environment: ENV,
+  release: `vectis-dapp@${pkj.version}`,
 });
